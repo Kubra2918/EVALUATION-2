6fb21ac8fc5c6b3c13381cde9e037303bb26855c
Ton nom <kubra.kurnaz@etu.univ-tours.fr>
2026-03-26 11:21:28 +0100

    KK - Ajout tableau page 2

--- pages/page2.py
+++ pages/page2.py
@@ -1,6 +1,18 @@
 import dash
-from dash import html
+from dash import html, dash_table
+import pandas as pd
 
 dash.register_page(__name__, path="/page2", name="Page 2")
 
-layout = html.Div("Page 2")
+df = pd.read_csv("datas/avocado.csv")
+
+layout = html.Div([
+    html.H1("Page 2- Tableau des données"),
+
+    dash_table.DataTable(
+       id= "table",
+       columns=[{"name": i, "id": i} for i in df.columns],
+       data=df.head(20).to_dict("records"),
+       page_size=10 
+    )
+])
