406ca6882c68d7aed0c217138a96d0fa4ff54163
Ton nom <kubra.kurnaz@etu.univ-tours.fr>
2026-03-26 11:06:57 +0100

    KK - Ajout graphique simple page 1

--- pages/page1.py
+++ pages/page1.py
@@ -1,6 +1,15 @@
 import dash
-from dash import html
+from dash import html, dcc
+import pandas as pd
+import plotly.express as px
 
 dash.register_page(__name__, path="/", name="Page 1")
 
-layout = html.Div("Page 1")
+df = pd.read_csv("datas/avocado.csv")
+
+fig = px.line(df, x="Date", y="AveragePrice", title="Prix moyen des avocats")
+
+layout = html.Div([
+    html.H1("Page 1- Analyse des prix"),
+    dcc.Graph(figure=fig)
+])
